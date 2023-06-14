005b040120486f791589c42e00d3c4caf854ceb2
Leroi <67743669+LeroiDev@users.noreply.github.com>
2023-06-14 16:48:19 +0200

    day3-end

--- js_fundamentals/03-Developer-Skills/starter/script.js
+++ js_fundamentals/03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
+function PrintForeCast(arr){
+    let celciusString = "";
+    for(let i = 0; i < arr.length; i++){
+        celciusString += `...${arr[i]}°C in ${i+1} days`;
+    }
+    return celciusString;
+}
+let testArray1 = [17,21,23];
+let testArray2 = [12,5,-5,0,4];
+console.log(PrintForeCast(testArray1));
+console.log(PrintForeCast(testArray2));
